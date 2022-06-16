7dfd032b03c021d436954f138ba919a1e1e26d89
bb <b@b.com>
2022-06-16 13:47:32 +0200

    bb -final- 2022-16-6

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "12345");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development Without EJB", "5555");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        Publisher cfg = new Publisher("CFG", "J.Gagarina", "Beograd", "Srbija", "11000");
+        cfg.getBooks().add(noEJB);
+        cfg.getBooks().add(ddd);
+        publisherRepository.save(cfg);
+
+        System.out.println("Started in BootStrap");
+        System.out.println("Number of books :" + bookRepository.count());
+        System.out.println("Number of publishers :" + publisherRepository.count());
+        System.out.println("Number of books published by " + cfg.getName() +": " + cfg.getBooks().size());
+
+    }
+}
